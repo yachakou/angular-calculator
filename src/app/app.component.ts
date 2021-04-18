0bf50258eb6bb146a33610c8558f3db50e2f901f
Yacine Chakour <yacine.veg@gmail.com>
2021-04-18 18:25:41 -0400

    Creation des bouttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,146 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-calculator';
+
+  numbers: Tile[] = [{
+    col:1,
+    row:1,
+    value: '7',
+    type: TypeButton.VALUE
+
+  },{
+    col:1,
+    row:1,
+    value: '8',
+    type: TypeButton.VALUE
+  },
+  {
+    col:1,
+    row:1,
+    value: '9',    
+    type: TypeButton.VALUE
+  },
+  {
+    col:1,
+    row:1,
+    value: '4',    
+    type: TypeButton.VALUE
+  },
+  {
+    col:1,
+    row:1,
+    value: '5',    
+    type: TypeButton.VALUE
+  },
+  {
+    col:1,
+    row:1,
+    value: '6',    
+    type: TypeButton.VALUE
+  },
+  {
+    col:1,
+    row:1,
+    value: '1',    
+    type: TypeButton.VALUE
+  },
+  {
+    col:1,
+    row:1,
+    value: '2',    
+    type: TypeButton.VALUE
+  },
+  {
+    col:1,
+    row:1,
+    value: '3',    
+    type: TypeButton.VALUE
+  },
+  {
+    col:2,
+    row:1,
+    value: '0',    
+    type: TypeButton.VALUE
+  },
+  {
+    col:1,
+    row:1,
+    value: '.',    
+    type: TypeButton.VALUE
+  }];
+  operators: Tile[] = [
+  {
+    col:1,
+    row:1,
+    value: 'X',
+    type: TypeButton.OPERATOR
+  },
+  {
+    col:1,
+    row:1,
+    value: '-',
+    type: TypeButton.OPERATOR
+  },
+  {
+    col:1,
+    row:1,
+    value: '+',
+    type: TypeButton.OPERATOR
+  },
+  {
+    col:1,
+    row:1,
+    value: '=',
+    type: TypeButton.OPERATOR
+  }
+];
+  other: Tile[] = [{
+    col:1,
+    row:1,
+    value: 'AC',
+    type: TypeButton.OTHER
+  },
+  {
+    col:1,
+    row:1,
+    value: '+/-',
+    type: TypeButton.OTHER
+  },
+  {
+    col:1,
+    row:1,
+    value: '%',
+    type: TypeButton.OTHER
+  }];
+  divide: Tile = {
+    col:1,
+    row:1,
+    value: "/",
+    type: TypeButton.OPERATOR
+  };
+  result = 0;
+
+
+
+
+
+
+
+}
+
+
+
+
+export interface Tile{
+  col: number,
+  row: number,
+  value : string,
+  type: TypeButton
+}
+
+export enum TypeButton {
+    OPERATOR = 'OPERATOR',
+    VALUE = 'VALUE',
+    OTHER = 'OTHER'
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './button/button.component';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatGridListModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
